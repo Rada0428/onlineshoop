cf82f02d130735d3542ff728a56cfe35177e262f
Rada0428 <amelus26@gmail.com>
2024-06-25 16:34:48 +0400

    first commit

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from '../servises/http.service';
+import { CartService } from '../servises/cart.service';
+import { DetailService } from '../servises/detail.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css',
+})
+export class DetailsComponent {
+  public selectedProduct!: any;
+
+  constructor(public route: ActivatedRoute, public service: HttpService, private cartService: CartService, private detailService: DetailService) {
+    this.selectedProduct = this.detailService.getProduct();
+    console.log(this.selectedProduct);
+  }
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+  }
+}
